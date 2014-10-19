6b932671afc938e5d95419afe3549d6a27ed644f
vickiedam90 <vickie@student.chalmers.se>
2014-10-19 03:31:01 +0200

    Changed server according to clients side

--- src/MyServer.java
+++ src/MyServer.java
@@ -1,120 +1,153 @@
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import org.json.JSONObject;
 
 public class MyServer {
 
-	//InetAddress infoip;
+	// InetAddress infoip;
 	private String message = "";
 	private ServerSocket serverSocket = null;
-	private List<Object> list = new ArrayList<>();
-
+	private Set<String> list = new HashSet<>();
+	
 	public MyServer() {
-	/*	try {
-			infoip = InetAddress.getLocalHost();
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		}*/
+		/*
+		 * try { infoip = InetAddress.getLocalHost(); } catch
+		 * (UnknownHostException e) { e.printStackTrace(); }
+		 */
 		Thread socketServerThread = new Thread(new SocketServerThread());
 		socketServerThread.start();
 	}
 
 	protected class SocketServerThread extends Thread {
-		ObjectInputStream in = null;
+
 		static final int SocketServerPORT = 8080;
-		
+
 		public void run() {
+			ObjectInputStream in = null;
 			Socket socket = null;
-			try {
-				serverSocket = new ServerSocket(SocketServerPORT);
-				System.out.println("Server waiting..");
-				
-				while (true) {
+			serverSocket = null;
+			InputStream is = null;
+
+			while (true) {
+				try {
+
+					in = null;
+					socket = null;
+					serverSocket = null;
+					is = null;
+
+					serverSocket = new ServerSocket(SocketServerPORT);
+					System.out.println("Server waiting..");
+
 					socket = serverSocket.accept();
 					System.out.println("Client detected");
-					//in = new ObjectInputStream(socket.getInputStream());
-					//list.add(in.readObject());
-					message += "From " + socket.getInetAddress() + ":"
-							+ socket.getPort() + "\n";
-					System.out.println(message);
-					SocketServerReplyThread socketServerReplyThread = new SocketServerReplyThread(
-							socket);
-					socketServerReplyThread.run();
 
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-		//	} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} finally {
-				if (serverSocket != null) {
-					try {
-						serverSocket.close();
-					} catch (IOException e) {
-						e.printStackTrace();
+					is = socket.getInputStream();
+					
+					in = new ObjectInputStream(is);
+					
+					list.addAll((Collection<? extends String>) in.readObject());
+					Thread.sleep(1000);
+					message += "From " + socket.getInetAddress() + ":"
+								+ socket.getPort() + "\n";
+					System.out.println(message);
+					System.out.println(list);
+					SocketServerReply socketServerReply = new SocketServerReply(
+								socket);
+					socketServerReply.run();
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (ClassNotFoundException e) {
+					e.printStackTrace();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					if (is != null) {
+						try {
+							is.close();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
 					}
-				}
-				if(socket != null){
-					try {
-						socket.close();
-					} catch (IOException e) {
-						e.printStackTrace();
+					if (in != null) {
+						try {
+							in.close();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
 					}
-				}
-				if(in != null){
-					try {
-						in.close();
-					} catch (IOException e) {
-						e.printStackTrace();
+					if (serverSocket != null) {
+						try {
+							serverSocket.close();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
 					}
+					if (socket != null) {
+						try {
+							socket.close();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+
 				}
 			}
 		}
 
 	}
 
-	private class SocketServerReplyThread extends Thread {
+	private class SocketServerReply {
 
 		private Socket hostThreadSocket;
+		String msgReply = "";
 
-		SocketServerReplyThread(Socket socket) {
+		SocketServerReply(Socket socket) {
 			hostThreadSocket = socket;
 		}
 
 		public void run() {
-			DataOutputStream out = null;
-			String msgReply = "Synchronization complete";
+			OutputStream out = null;
+			msgReply = "Synchronization complete";
 			try {
-				out = new DataOutputStream(hostThreadSocket.getOutputStream());
-				out.writeUTF(msgReply);
-				message += "replayed: " + msgReply + "\n";
 
+				out = hostThreadSocket.getOutputStream();
+				PrintStream printStream = new PrintStream(out);
+				//out.writeUTF(msgReply);
+				printStream.print(msgReply);
+				message += "replayed: " + msgReply + "\n";
+				out.flush();
 			} catch (IOException e) {
 				e.printStackTrace();
 				message += "Something wrong! " + e.toString() + "\n";
 			} finally {
-				if (hostThreadSocket != null) {
+				msgReply = "";
+				message = "";
+
+				if (out != null) {
 					try {
-						hostThreadSocket.close();
+						out.close();
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
 				}
-				if (out != null) {
+				if (hostThreadSocket != null) {
 					try {
-						out.close();
+						hostThreadSocket.close();
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
@@ -130,4 +163,3 @@ public class MyServer {
 	}
 
 }
-
